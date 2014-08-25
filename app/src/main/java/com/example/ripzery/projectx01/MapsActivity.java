f20c50db19a538dd84ce82d2dcff299b2d8c8725
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2014-08-25 17:49:32 +0700

    add ProgressDialog add animation when move camera

--- app/src/main/java/com/example/ripzery/projectx01/MapsActivity.java
+++ app/src/main/java/com/example/ripzery/projectx01/MapsActivity.java
@@ -1,20 +1,16 @@
 package com.example.ripzery.projectx01;
 
-import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
+import android.app.ProgressDialog;
 import android.hardware.Sensor;
 
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -25,39 +21,50 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements SensorEventListener, LocationListener{
+public class MapsActivity extends FragmentActivity implements SensorEventListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LatLngBounds Mahidol = new LatLngBounds(new LatLng(13.787486,100.316179),new LatLng(13.800875,100.326897));
     private SensorManager mSensorManager;
-    private ImageView image;
-    MarkerOptions arrow;
-    Marker dirArrow;
+    MarkerOptions mArrow;
+    Marker mDirArrow;
     BitmapDescriptor bd;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
         setUpMapIfNeeded();
+
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
         mMap.getUiSettings().setCompassEnabled(true);
-        setMapCenter(Mahidol);
+        setMapCenter(Mahidol.getCenter(),15);
+
+        progress =  new ProgressDialog(this);
+        progress = ProgressDialog.show(this, "Loading", "Wait while loading map...");
+
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                progress.setMessage("Wait while getting your location");
+            }
+        });
 
         mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
-                if(arrow == null){
-                    arrow = new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude()))
+                if(mArrow == null){
+                    mArrow = new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude()))
                             .title("Current Location")
                             .icon(bd);
-                    dirArrow = mMap.addMarker(arrow);
+                    mDirArrow = mMap.addMarker(mArrow);
+                    setMapCenter(mArrow.getPosition(),17);
+                    progress.dismiss();
                 }else{
-                    dirArrow.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+                    mDirArrow.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
                 }
-
             }
         });
 
@@ -80,9 +87,9 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
         // get the angle around the z-axis rotated
         float degree = Math.round(event.values[0]);
 
-        if(dirArrow != null){
-            dirArrow.setAnchor((float) 0.5, (float) 0.5);
-            dirArrow.setRotation(degree);
+        if(mDirArrow != null){
+            mDirArrow.setAnchor((float) 0.5, (float) 0.5);
+            mDirArrow.setRotation(degree);
         }
 
 //        Log.d("Degrees : ",""+degree);
@@ -94,8 +101,9 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
 
     }
 
-    private void setMapCenter(LatLngBounds Location){
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Mahidol.getCenter(), 15));
+    private void setMapCenter(LatLng Location,int zoomLevel){
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(Location, zoomLevel);
+        mMap.animateCamera(update);
     }
 
     @Override
@@ -107,21 +115,6 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
                 SensorManager.SENSOR_DELAY_GAME);
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -135,33 +128,9 @@ public class MapsActivity extends FragmentActivity implements SensorEventListene
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
+
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d("Hello","Hello");
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 }
